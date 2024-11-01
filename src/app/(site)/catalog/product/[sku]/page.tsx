d02048304963b89f4c1be80423de2560b1c0009e
Deadmanch <stepanova.nata.r@gmail.com>
2024-11-01 15:30:15 +0300

    feature - добавил получение товаров и генерации страниц SSG

--- src/app/(site)/catalog/product/[sku]/page.tsx
+++ src/app/(site)/catalog/product/[sku]/page.tsx
@@ -1,4 +1,14 @@
+import { getProducts } from '@/api/requests'
 import { Title } from '@/components'
+import { IProductsResponse } from '@/interfaces/products.interface'
+
+export async function generateStaticParams() {
+  const { products }: IProductsResponse = await getProducts({
+    limit: 1000,
+    offset: 0
+  })
+  return products.map((product) => ({ sku: String(product.sku) }))
+}
 
 export default function Product() {
   return <Title>Название товара</Title>
