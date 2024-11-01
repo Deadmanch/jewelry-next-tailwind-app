2ee8d06af4b28e92544386424d039716e0dc88d8
Deadmanch <stepanova.nata.r@gmail.com>
2024-11-01 15:37:41 +0300

    feature - вынес все константы в отдельный файл

---
+++ src/constants/constants.ts
@@ -0,0 +1,5 @@
+export const DEFAULT_LIMIT_PRODUCTS = 6
+export const MAXIMUM_LIMIT_PRODUCTS = 1000
+export const DEFAULT_PAGE = 1
+export const DEFAULT_MIN_PRICE = 0
+export const DEFAULT_MAX_PRICE = 1000
