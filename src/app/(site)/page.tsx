01ac34c63b30bffc1df0003e962bfe623abcfd97
Deadmanch <stepanova.nata.r@gmail.com>
2024-11-01 15:34:46 +0300

    feature - добавил отрисовку данных для главной страницы

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,5 +1,27 @@
-import { Title } from '@/components'
+import Link from 'next/link'
 
-export default function Home() {
-  return <Title>Home</Title>
+import { getProducts } from '@/api'
+import { HeroBlock, ProductList, Title } from '@/components'
+import { DEFAULT_LIMIT_PRODUCTS } from '@/constants'
+import { NavigationRouting } from '@/mock/navigation-routing'
+import { IProductsResponse } from '@/interfaces'
+export default async function Home() {
+  const { products }: IProductsResponse = await getProducts({
+    limit: DEFAULT_LIMIT_PRODUCTS,
+    offset: 0
+  })
+  return (
+    <>
+      <HeroBlock products={products} className='my-5 2xl:mb-12' />
+      <section className='mb-5 flex flex-col gap-10 px-4 2xl:mb-20 2xl:px-0'>
+        <div className='flex items-center justify-between'>
+          <Title tag='h4'>Последние поступления</Title>
+          <Link className='text-primary hover:text-black' href={NavigationRouting.CATALOG}>
+            Все
+          </Link>
+        </div>
+        <ProductList className='lg:grid-cols-3' products={products} />
+      </section>
+    </>
+  )
 }
